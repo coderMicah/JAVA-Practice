36108fb520e8a7f2e45502ed7ce7e8af7ba03069
Micah <ismiki977@gmail.com>
2025-06-23 14:16:30 +0300

    first commit

---
+++ TYPES/Demo.java
@@ -0,0 +1,21 @@
+package TYPES;
+
+class Demo {
+    public static void main(String args[]) {
+
+        // Primitive Data Types
+        byte b = 1;
+        short s = 22;
+        int i = 123;
+        long l = 137613;
+
+        float f = 1.34f;
+        double d = 1.34;
+
+        char c = 'c';
+
+        boolean bl = true;
+
+        System.out.println("Hello World");
+    }
+}
